d100a0d77d66ef58c8d585b7b23ee63d4777f4eb
Florian Pürschel <hello@prshl.de>
2024-03-18 17:32:59 +0100

    a lot of boilerplate

---
+++ sanity-codegen.config.ts
@@ -0,0 +1,8 @@
+import { SanityCodegenConfig } from "sanity-codegen";
+
+const config: SanityCodegenConfig = {
+  schemaPath: "./sanity/schema.ts",
+  outputPath: "./types/sanity.ts",
+};
+
+export default config;
