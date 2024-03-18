ed5cfe6bafc2ff4fa65a444a2215cd97d85cc4cb
Florian Pürschel <hello@prshl.de>
2024-03-18 17:58:50 +0100

    add grtsk

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--font-sans)", ...fontFamily.sans],
+        grtsk: ["var(--font-grtsk)", ...fontFamily.sans],
       },
     },
   },
